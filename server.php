c0b41dc740738e69096934e2c7b1358c884a0a15
pankpan <pank@pank.org>
2024-09-24 22:28:10 +0800

    add client side command tip

--- server.php
+++ server.php
@@ -14,15 +14,16 @@ if (!($sock = socket_create(AF_INET, SOCK_DGRAM, 0))) {
     $errormsg = socket_strerror($errorcode);
     die("Couldn't create socket: [$errorcode] $errormsg \n");
 }
-echo "Socket created \n";
+echo "Socket created\n";
 
 if (!socket_bind($sock, "0.0.0.0", $argv[1])) {
     $errorcode = socket_last_error();
     $errormsg = socket_strerror($errorcode);
     die("Could not bind socket : [$errorcode] $errormsg \n");
 }
-echo "Socket bind OK \n";
-
+echo "Socket bind OK\n";
+$ip=trim(file_get_contents('http://icanhazip.com'));
+echo "Client side command: php client.php $ip $port\n\n";
 while (true) {
     echo "Waiting for data ... \n";
     $r = socket_recvfrom($sock, $buf, 512, 0, $remote_ip, $remote_port);

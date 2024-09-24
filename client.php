6a3f0af053b4e281eb1dd08cba651b9a25eb8372
pankpan <pank@pank.org>
2024-09-25 01:44:40 +0800

    add comment

--- client.php
+++ client.php
@@ -11,13 +11,13 @@ if (!($sock = socket_create(AF_INET, SOCK_DGRAM, 0))) {
     $errormsg = socket_strerror($errorcode);
     die("Couldn't create socket: [$errorcode] $errormsg \n");
 }
-$id=rand(1000,9999);
+$id=rand(1000,9999); // random id to identify client
 $key='hello';
-$buf=$key.'-'.$id;
+$msg=$key.'-'.$id;
 echo "This node id: $id\n";
 while (true) {
-    echo "Send $buf to $server:$port\n";
-    if (!socket_sendto($sock, $buf , strlen($buf) , 0 , $server , $port)) {
+    echo "Send $msg to $server:$port\n";
+    if (!socket_sendto($sock, $msg , strlen($msg) , 0 , $server , $port)) {
         $errorcode = socket_last_error();
         $errormsg = socket_strerror($errorcode);
         die("Could not send data: [$errorcode] $errormsg \n");
@@ -28,11 +28,11 @@ while (true) {
         die("Could not receive data: [$errorcode] $errormsg \n");
     }
     echo "Got: $reply\n";
-    if (strstr($reply,':')) { // got peer ip port
+    if (strstr($reply,':')) { // got peer ip:port
         $arr=explode(':',$reply);
-        $server=$arr[0];
-        $port=$arr[1];
-        $buf='peer-'.$id; // change send data to peer-id
+        $server=$arr[0]; // change destnation to peer
+        $port=$arr[1]; // change to peer's port
+        $msg='peer-'.$id; // change send data to peer-id
     }
     sleep(1);
 }
